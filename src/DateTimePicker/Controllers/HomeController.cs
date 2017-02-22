8c8ec4e73000e24896519e2bbd27392ced52e12a
Niels Odgaard <nod@novicell.dk>
2017-02-22 14:01:14 +0100

    Jquery DateTimePicker works

--- src/DateTimePicker/Controllers/HomeController.cs
+++ src/DateTimePicker/Controllers/HomeController.cs
@@ -31,5 +31,10 @@ namespace DateTimePicker.Controllers
         {
             return View();
         }
+
+        public IActionResult DateTimePicker()
+        {
+            return View();
+        }
     }
 }
